0c58aa26971e069b55c8549b44e96c485c121b35
GitHolo <ooye2332@gmail.com>
2023-04-06 20:12:11 +0200

    GRADINETT WTFFF + scrollbar

--- logic.js
+++ logic.js
@@ -4,22 +4,16 @@ function startButton() {
   const contentBackground = document.querySelector(".content-background");
   const contentTop =
     contentBackground.getBoundingClientRect().top + window.pageYOffset;
-
   window.scrollTo({
     top: contentTop,
     behavior: "smooth",
   });
 }
 function search() {
-  // Select the search input and fish items
   const searchInput = document.getElementById("search");
   const fishItems = document.querySelectorAll(".fish-item");
-
-  // Add an event listener to the search input
   searchInput.addEventListener("input", () => {
     const searchValue = searchInput.value.toLowerCase();
-
-    // Loop through the fish items and hide/show them based on the input value
     fishItems.forEach((item) => {
       const fishName = item
         .querySelector(".fish-name")
